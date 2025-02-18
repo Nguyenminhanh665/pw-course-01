44fd22d81f9f40518318627039e9dca4a853dcc5
PW - Minh Anh <minhhnguyen665@gmail.com>
2025-02-18 18:44:03 +0700

    add bài tập lesson-2

---
+++ Ví-dụ.js
@@ -0,0 +1,23 @@
+// // // // 
+
+// // // const number = 160;
+
+// // // const result1 = number * 9 / 10;
+// // // const result2 = number * 1;
+// // // const result3 = number * 8 / 10;
+
+// // // console.log(result1);
+// // // console.log(result2);
+// // // console.log(result3);
+
+// // // console.log(result1 + result2 + result3);
+
+// // let firstName = "Ánh";
+// // let lastName = "Minh";
+// //  console.log(lastName , firstName);
+
+// for (let i = 1002; i <= 1998; i++) {
+//     if (i % 3 === 0); {
+//         console.log("Giá trị của i là: ", i);
+//     }
+// }
