8873ca5b338d49e2b01c05ba3ad5a179492c13cc
PW - Minh Anh <minhhnguyen665@gmail.com>
2025-02-23 01:46:19 +0700

    update lesson-3

---
+++ tests/lesson-3/03-for.js
@@ -0,0 +1,22 @@
+let arr11 = [];
+for (let i = 1; i <= 100; i++) {
+    console.log(arr[i - 1] = i);
+}
+console.log("Mảng số từ 1 đến 100: ", arr);
+ let sum = 0;
+ for (let i = 1; i <= 100; i++) {
+    sum = sum +i;
+ }
+ console.log("Tổng các số từ 1 đến 100: ", sum);
+//
+for (let i = 2; i <= 9; i++) {
+    for ( let j = 1; j <= 10; j++) {
+        console.log(`${i} * ${j} = ${i * j}`);
+    } 
+}
+// 
+let arr22 = []
+for (let i = 1; i <= 99; i +=2) {
+    console.log(arr.push(i));
+}
+console.log("Mảng số lẻ từ 1 đến 99 là: ", arr);
