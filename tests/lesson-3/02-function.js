1852368d9da0fed2671af31eaaf46f5e42bfd1df
PW - Minh Anh <minhhnguyen665@gmail.com>
2025-02-24 02:56:11 +0700

    continue update lesson-3

--- tests/lesson-3/02-function.js
+++ tests/lesson-3/02-function.js
@@ -14,7 +14,33 @@ function tinhChiSoBMI(height, weight)  {
         return("Beo phi");
     }
 }
-const result = tinhChiSoBMI(1.70, 50);
-console.log(result);
+const result1 = tinhChiSoBMI(1.70, 50);
+console.log(result1);
 //
 2.
+function translateTemperature(temperature, type) {
+    let doF = temperature * 9/5 + 32
+    let doC = (temperature - 32) * 5 / 9
+    if(type === 'C') {
+        console.log(`Nhiệt độ chuyển đổi là ${doF}`);
+    }
+    if(type === 'F') {
+        console.log(`Nhiệt độ chuyển đổi là ${doC}`);
+    }
+}
+const result2 = translateTemperature(40, 'C');
+console.log(result2);
+//
+3.
+const arr = [3, 4, 7, 5, 8, 9]
+let sum = 0
+function tongSo(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+const result = tongSo(arr)
+console.log(result);
+//
+4. 
