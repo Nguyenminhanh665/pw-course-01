be250c8a7c8d2f559ff7ee7ec2cb953418b23736
PW - Minh Anh <minhhnguyen665@gmail.com>
2025-03-01 00:13:13 +0700

    finished lesson-4

---
+++ tests/lesson-4/05-for.js
@@ -0,0 +1,35 @@
+1.// In ten va gia tri cua thuoc tinh trong 1 doi tuong
+const personal = {
+    "name": "Minh Anhh",
+    "age": 18,
+    "school": "AOF"
+}
+for(let feature in personal) {
+    console.log(`${feature}=${personal[feature]}`);
+}
+2.// Tính tổng các giá trị số của các thuộc tính của 1 đối tượng
+const product = {"banana": 20, "orange": 18, "origin": "Japan"};
+let sum = 0;
+for(let property in product) {
+    if(typeof product[property] === "number" ) {
+        sum += product[property];
+    } else {
+        continue;
+    }
+}
+console.log(sum);
+3.// Tạo 1 mảng chứa tất cả các thuộc tính của một đối tượng
+const student = {"name": "Anhh", "school": "AOF"};
+const newStudent = [];
+for(let information in student) {
+    newStudent.push(information);
+}
+console.log(newStudent);
+4.//Tạo mảng chứa các ký tự nghịch đảo từ 1 chuỗi đã cho
+const string = "Google";
+let newString = [];
+for(let char of string) {
+    newString.unshift(char);
+}
+console.log(newString);
+
--- tests/lesson-4/test-1.js
+++ tests/lesson-4/test-1.js
@@ -1,7 +1,7 @@
 1.// Khởi động Tàu vũ trụ K12
 let departureEarth = "Trái Đát";
 let mission = "Khám phá vũ trụ K12";
-let crew = ["Candy Vie ", "Hạnh", "Minh Châu", "Thùy An", "Mai Uyên", "Minh", "Thảo Vy", "Thanh Thiên", "Khánh Vân", "Nhật Khánh", "Thảo Đoàn", "Lê My", "Minh Trang" ];
+let crew = ["Candy Vie ", "Hạnh", "Minh Châu", "Thùy An", "Mai Uyên", "Minh", "Thảo Vy", "Thanh Thiên", "Khánh Vân", "Nhật Khánh", "Thảo Đoàn", "Lê My", "Minh Trang"];
 function launchShip() {
     console.log(`Chuẩn bị khởi động! Phi hành đoàn gồm: ${crew} sẽ đồng hành cùng bạn trong chuyến phiêu lưu ${mission}`);
     return;
@@ -17,16 +17,16 @@ caculatedDistance(1000, 24);
 3.// Hành tinh kỳ lạ
 function convertTimeToHex(time) {
     let hexadecimalNumber = time.toString(16);
-    return(`Thời gian trên hành tinh bí ẩn là ${hexadecimalNumber} giây`);
+    return (`Thời gian trên hành tinh bí ẩn là ${hexadecimalNumber} giây`);
 }
 const result = convertTimeToHex(120);
 console.log(result);
 4.// Khám phá kho báu
-let originCode =  "K12 Challenging";
+let originCode = "K12 Challenging";
 let newCode = "";
 function decryptCode(code) {
-    for(let i = 0; i < code.length; i++) {
-        if(code[i] === code[i].toUpperCase()) {
+    for (let i = 0; i < code.length; i++) {
+        if (code[i] === code[i].toUpperCase()) {
             newCode += code[i].toLowerCase();
         } else {
             newCode += code[i].toUpperCase();
