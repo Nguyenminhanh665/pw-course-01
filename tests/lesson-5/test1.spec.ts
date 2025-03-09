a742d546c3a9bae7a5aa1d07d9a65d73f7c21d9f
PW - Minh Anh <minhhnguyen665@gmail.com>
2025-03-10 00:58:55 +0700

    chữa bài tập lesson-5

--- tests/lesson-5/test1.spec.ts
+++ tests/lesson-5/test1.spec.ts
@@ -1,27 +1,41 @@
 import { test } from '@playwright/test'
 
-test('Bài 1', async ({ page }) => {
+//Khai báo data trước để khi viết test case sẽ dễ gọi, tránh lặp data, code gọn hơn và dễ update về sau
+const date = "2005-04-28";
+const username = "Nguyễn Minh Ánh";
+const email = "minhhnguyen665@gmail.com";
+const description = "Tôi là Minh Ánh, đến từ AOF";
+const country = "canada";
+
+test('Bài 1: Register Page', async ({ page }) => {
     await test.step('Navigate to material playwright', async () => {
         await page.goto('https://material.playwrightvn.com/')
     });
 
-    await test.step('Click vào bài 1: Register Page', async () => {
+    await test.step('Click on User Registration', async () => {
         await page.locator(`//a[@href='01-xpath-register-page.html']`).click();
     });
 
-    await test.step('Fill information', async () => {
-        await page.locator(`//input[@id='username']`).fill('Minh Ánh');
-        await page.locator(`//input[@id='email']`).fill('minhhnguyen665@gmail.com');
-        await page.locator(`//input[@id='female']`).setChecked(true);
-        await page.locator(`//input[@id='reading']`).setChecked(true);
+    await test.step('Fill information to all fields', async () => {
+        await page.locator(`//input[@id='username']`).fill(username);
+        await page.locator(`//input[@id='email']`).fill(email);
+        await page.locator(`//input[@id='female']`).check();
+        await page.locator(`//input[@id='reading']`).check();
         await page.locator(`//select[@id='interests']`).selectOption({ value: 'art' });
         await page.locator(`//select[@id='country']`).selectOption({ value: 'canada' });
-        await page.locator(`//input[@id='dob']`).fill('2005-04-28');
+        //await page.selectOption(`//select[@id='country']`, country); Cách 22
+        await page.locator(`//input[@id='dob']`).fill(date);
         await page.locator(`//input[@id='profile']`).setInputFiles('data-test/tải xuống (1).png');
-        await page.locator(`//textarea[@id='bio']`).fill('Tôi là Minh Ánh, đến từ AOF');
-        await page.locator(`//input[@id='newsletter']`).setChecked(true);
-        await page.locator(`label[for='toggleOption']`).click();
-        await page.locator(`//button[text()='Register']`).click();
+        await page.locator(`//textarea[@id='bio']`).fill(description);
+        await page.locator(`//div[@class='tooltip']`).hover();
+        await page.locator(`//input[@id='newsletter']`).check();
+        debugger;
+        //await page.click(`//label[@class = "switch"]`);
+        await page.click(`//span[@class = "slider round"]`);
+    })
+
+    await test.step("Click Register button", async () => {
+        await page.locator(`//button[normalize-space()='Register']`).click();
     })
 })
 
--- tests/lesson-5/test2.spec.ts
+++ tests/lesson-5/test2.spec.ts
@@ -1,20 +1,31 @@
 import { test } from '@playwright/test'
 
-test('Bài 2', async ({ page }) => {
+
+test('Exercise 2: Product PagePage', async ({ page }) => {
     await test.step('Navigate to material playwright', async () => {
         await page.goto('https://material.playwrightvn.com/')
-    });
+    })
+
     await test.step('Click vài bài 2: Product page', async () => {
         await page.locator(`//a[@href='02-xpath-product-page.html']`).click()
-    });
+    })
+        // await test.step('Add product', async () => {
+        // await page.locator(`//button[@data-product-id='1']`).dblclick();
+        // await page.locator(`//button[@data-product-id='2']`).click({ clickCount: 3 });
+        // await page.locator(`//button[@data-product-id='3']`).click();
+    await test.step("Add Product 1: 2 items", async () => {
+        await page.locator(`//button[@data-product-id="1"]`).dblclick();
+    })
 
-    await test.step('Add product', async () => {
-        await page.locator(`//button[@data-product-id='1']`).dblclick();
-        await page.locator(`//button[@data-product-id='2']`).click({ clickCount: 3 });
-        await page.locator(`//button[@data-product-id='3']`).click();
-    });
+    await test.step("Add Product 2: 3 items", async () => {
+        await page.locator(`//button[@data-product-id="2"]`).click({ clickCount: 3 });
+    })
 
-});
+    await test.step("Add Product 3: 1 items", async () => {
+        await page.locator(`//button[@data-product-id="3"]`).click();
+    })
+
+})
 
 
 
